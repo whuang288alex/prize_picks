9bebe40654f754242338c0745a8fc05aec77c35b
Alex <whuang288@wisc.edu>
2023-11-27 05:45:12 +0000

    cleaned up data fetching pipeline

--- app.py
+++ app.py
@@ -8,115 +8,19 @@ from nba_api.stats.static import teams
 from nba_api.stats.static import players
 
 
+STATS = ["minutes", "points", "field_goals_made", "field_goals_attempted", "three_point_field_goals_made", "three_point_field_goals_attempted", "free_throws_made", "free_throws_attempted", "offensive_rebounds", "defensive_rebounds", "total_rebounds", "assists", "steals", "blocks", "turnovers"]
+STATS_FOR_DISPLAY = ["Minutes", "Points", "Field Goals Made", "Field Goals Attempted", "3-Point Field Goals Made", "3-Point Field Goals Attempted", "Free Throws Made", "Free Throws Attempted", "Offensive Rebounds", "Defensive Rebounds", "Total Rebounds", "Assists", "Steals", "Blocks", "Turnovers"]
+TEAMS = ["Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets", "Chicago Bulls", "Cleveland Cavaliers", "Dallas Mavericks", "Denver Nuggets", "Detroit Pistons", "Golden State Warriors", "Houston Rockets", "Indiana Pacers", "Los Angeles Clippers", "Los Angeles Lakers", "Memphis Grizzlies", "Miami Heat", "Milwaukee Bucks", "Minnesota Timberwolves", "New Orleans Pelicans", "New York Knicks", "Oklahoma City Thunder", "Orlando Magic", "Philadelphia 76ers", "Phoenix Suns", "Portland Trail Blazers", "Sacramento Kings", "San Antonio Spurs", "Toronto Raptors", "Utah Jazz", "Washington Wizards"]
+TEAM_STATS = ["defensive_rating", "effective_defensive_rating", "pace", "opponent_score", "opponent_field_goals_made", "opponent_field_goals_attempted", "opponent_three_point_field_goals_made", "opponent_three_point_field_goals_attempted", "opponent_free_throws_made", "opponent_free_throws_attempted", "opponent_defensive_rebounds", "opponent_offensive_rebounds", "opponent_total_rebounds", "opponent_assists", "opponent_steals", "opponent_blocks", "opponent_turnovers"]
+TEAM_STATS_FOR_DISPLAY = ["Defensive Rating", "Effective Defensive Rating", "Pace", "Opponent Score", "Opponent Field Goals Made", "Opponent Field Goals Attempted", "Opponent 3-Point Field Goals Made", "Opponent 3-Point Field Goals Attempted", "Opponent Free Throws Made", "Opponent Free Throws Attempted", "Opponent Defensive Rebounds", "Opponent Offensive Rebounds", "Opponent Total Rebounds", "Opponent Assists", "Opponent Steals", "Opponent Blocks", "Opponent Turnovers"]
+
+
+# Set the width of the charts
 width = 500
 st.set_page_config(layout="wide")
 
-STATS = [
-    "minutes",
-    "points",
-    "field_goals_made",
-    "field_goals_attempted",
-    "three_point_field_goals_made",
-    "three_point_field_goals_attempted",
-    "free_throws_made",
-    "free_throws_attempted",
-    "offensive_rebounds",
-    "defensive_rebounds",
-    "total_rebounds",
-    "assists",
-    "steals",
-    "blocks",
-    "turnovers"
-]
-STATS_FOR_DISPLAY = [
-    "Minutes",
-    "Points",
-    "Field Goals Made",
-    "Field Goals Attempted",
-    "3-Point Field Goals Made",
-    "3-Point Field Goals Attempted",
-    "Free Throws Made",
-    "Free Throws Attempted",
-    "Offensive Rebounds",
-    "Defensive Rebounds",
-    "Total Rebounds",
-    "Assists",
-    "Steals",
-    "Blocks",
-    "Turnovers"
-]
-TEAMS = [
-    "Atlanta Hawks",
-    "Boston Celtics",
-    "Brooklyn Nets",
-    "Charlotte Hornets",
-    "Chicago Bulls",
-    "Cleveland Cavaliers",
-    "Dallas Mavericks",
-    "Denver Nuggets",
-    "Detroit Pistons",
-    "Golden State Warriors",
-    "Houston Rockets",
-    "Indiana Pacers",
-    "Los Angeles Clippers",
-    "Los Angeles Lakers",
-    "Memphis Grizzlies",
-    "Miami Heat",
-    "Milwaukee Bucks",
-    "Minnesota Timberwolves",
-    "New Orleans Pelicans",
-    "New York Knicks",
-    "Oklahoma City Thunder",
-    "Orlando Magic",
-    "Philadelphia 76ers",
-    "Phoenix Suns",
-    "Portland Trail Blazers",
-    "Sacramento Kings",
-    "San Antonio Spurs",
-    "Toronto Raptors",
-    "Utah Jazz",
-    "Washington Wizards"
-]
-TEAM_STATS = [
-    "defensive_rating",
-    "effective_defensive_rating",
-    "pace",
-    "opponent_score",
-    "opponent_field_goals_made",
-    "opponent_field_goals_attempted",
-    "opponent_three_point_field_goals_made",
-    "opponent_three_point_field_goals_attempted",
-    "opponent_free_throws_made",
-    "opponent_free_throws_attempted",
-    "opponent_defensive_rebounds",
-    "opponent_offensive_rebounds",
-    "opponent_total_rebounds",
-    "opponent_assists",
-    "opponent_steals",
-    "opponent_blocks",
-    "opponent_turnovers",
-]
-TEAM_STATS_FOR_DISPLAY = [
-    "Defensive Rating",
-    "Effective Defensive Rating",
-    "Pace",
-    "Opponent Score",
-    "Opponent Field Goals Made",
-    "Opponent Field Goals Attempted",
-    "Opponent 3-Point Field Goals Made",
-    "Opponent 3-Point Field Goals Attempted",
-    "Opponent Free Throws Made",
-    "Opponent Free Throws Attempted",
-    "Opponent Defensive Rebounds",
-    "Opponent Offensive Rebounds",
-    "Opponent Total Rebounds",
-    "Opponent Assists",
-    "Opponent Steals",
-    "Opponent Blocks",
-    "Opponent Turnovers",
-]
 
-# Create two columns for selecting player and team stats
+# Create two columns, one for player and one for team stats
 player_stats_col, team_stats_col = st.columns(2)
 conn = sqlite3.connect('database.db')
 c = conn.cursor()
@@ -129,9 +33,9 @@ with player_stats_col:
     # User input: Player name and stat category
     player_name = st.text_input("Enter player's name:", "LeBron James")
     stat_category = st.selectbox("Select a stat category:", STATS_FOR_DISPLAY, index=1)
-    last_n_games = st.slider("Select number of games:", 1, 15, 5)
+    last_n_games = st.slider("Select number of games:", 1, 15, 10)
     players_list = players.find_players_by_full_name(player_name)
-    home_game = st.selectbox("Home or Away?", ["All", "Home", "Away"], index=0)
+    home_game = st.selectbox("Home or Away:", ["All", "Home", "Away"], index=0)
 
     if len(players_list) > 0:
     
@@ -185,28 +89,33 @@ with player_stats_col:
 
 # Team Stats Selection in the second column
 with team_stats_col:
-    st.title("Team Stats")
+    st.title("Team Defensive Stats")
     team_name = st.selectbox("Select a team:", TEAMS, index=0)
     team_stats_category = st.selectbox("Select a stat category for team data:", TEAM_STATS_FOR_DISPLAY, index=1)
-    team_last_n_games = st.slider("Select number of games for team_data:", 1, 15, 5)
-    team_home_game = st.selectbox("Home or Away for team data?", ["All", "Home", "Away"], index=0)
+    team_last_n_games = st.slider("Select number of games for team_data:", 1, 15, 10)
+    team_home_game = st.selectbox("Home or Away for team data:", ["All", "Home", "Away"], index=0)
 
-    # Get team ID
+    # Get this stats for the team
     team_id = teams.find_teams_by_full_name(team_name)[0]['id']
     if team_home_game == "All":
-        c.execute(f'''SELECT game_date, {TEAM_STATS[TEAM_STATS_FOR_DISPLAY.index(team_stats_category)]} from TeamStats WHERE team_id={team_id} ORDER BY game_date DESC LIMIT {team_last_n_games}''')
+        home_game_clause = "(home_game = 1 OR home_game=0)"
     elif team_home_game == "Home":
-        c.execute(f'''SELECT game_date, {TEAM_STATS[TEAM_STATS_FOR_DISPLAY.index(team_stats_category)]} from TeamStats WHERE team_id={team_id} AND home_game=1 ORDER BY game_date DESC LIMIT {team_last_n_games}''')
+        home_game_clause = "home_game = 1"
     else:
-        c.execute(f'''SELECT game_date, {TEAM_STATS[TEAM_STATS_FOR_DISPLAY.index(team_stats_category)]} from TeamStats WHERE team_id={team_id} AND home_game=0 ORDER BY game_date DESC LIMIT {team_last_n_games}''') 
-    
-    
+        home_game_clause = "home_game = 0"
+    c.execute(f'''SELECT game_date, {TEAM_STATS[TEAM_STATS_FOR_DISPLAY.index(team_stats_category)]} from TeamStats WHERE team_id={team_id} AND {home_game_clause} ORDER BY game_date DESC LIMIT {team_last_n_games}''')
     team_stats = c.fetchall()
     conn.commit()
 
+    # baseline 
+    c.execute(f'''SELECT {TEAM_STATS[TEAM_STATS_FOR_DISPLAY.index(team_stats_category)]} from TeamStats''') 
+    this_stat_for_all_games = c.fetchall()
+    this_stat_mean = np.mean(this_stat_for_all_games)
+    this_stat_std = np.std(this_stat_for_all_games)
+        
     # Create a list of the stats
     team_dates = [stat[0] for stat in team_stats]
-    team_data = [stat[1] for stat in team_stats]
+    team_data = [(stat[1]- this_stat_mean)/this_stat_std for stat in team_stats]
 
     # pick a threshold, default to the mean of the stats
     team_threshold = st.slider("Select a threshold for team data:", 0.0, 50.0, np.mean(team_data), 0.5)
